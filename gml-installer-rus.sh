bebcac8520020a9510ebf718fb84d5507db67bd8
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-04-26 18:12:03 +0300

    Update gml-installer-rus.sh

--- gml-installer-rus.sh
+++ gml-installer-rus.sh
@@ -2,184 +2,128 @@
 
 if [ "$(id -u)" -ne 0 ]
 then
-  echo "Необходимо запустить скрипт под root пользователем"
+  echo "The script needs to be run as root"
   exit 1
 fi
 
 
 #!/bin/bash
 
-# Проверка установки git
+# Check for git installation
 if ! command -v git > /dev/null
 then
-    echo "[Git] Git не найден. Попытка установить..."
+    echo "[Git] Git not found. Attempting to install..."
     apt-get install -y git
     if [ $? -eq 0 ]
     then
-        echo "[Git] Успешно установлен"
+        echo "[Git] Installation successful"
     else
-        echo "[Git] Не удалось установить Git. Пожалуйста, установите его вручную."
+        echo "[Git] Failed to install Git. Please install it manually."
         exit 1
     fi
 else
-    echo "[Git] Установлен"
+    echo "[Git] Installed"
 fi
 
-# Проверка установки jq
+# Check for jq installation
 if ! command -v jq > /dev/null
 then
-    echo "[jq] jq не найден. Попытка установить..."
+    echo "[jq] jq not found. Attempting to install..."
     apt-get install -y jq
     if [ $? -eq 0 ]
     then
-        echo "[jq] Успешно установлен"
+        echo "[jq] Installation successful"
     else
-        echo "[jq] Не удалось установить jq. Пожалуйста, установите его вручную."
+        echo "[jq] Failed to install jq. Please install it manually."
         exit 1
     fi
 else
-    echo "[jq] Установлен"
+    echo "[jq] Installed"
 fi
 
-# Проверка установки curl
+# Check for curl installation
 if ! command -v curl > /dev/null
 then
-    echo "[Curl] Curl не найден. Попытка установить..."
+    echo "[Curl] Curl not found. Attempting to install..."
     apt-get install -y curl
     if [ $? -eq 0 ]
     then
-        echo "[Curl] Успешно установлен"
+        echo "[Curl] Installation successful"
     else
-        echo "[Curl] Не удалось установить Curl. Пожалуйста, установите его вручную."
+        echo "[Curl] Failed to install Curl. Please install it manually."
         exit 1
     fi
 else
-    echo "[Curl] Установлен"
+    echo "[Curl] Installed"
 fi
 
-# Проверка установки wget
+# Check for wget installation
 if ! command -v wget > /dev/null
 then
-    echo "[Wget] Wget не найден. Попытка установить..."
+    echo "[Wget] Wget not found. Attempting to install..."
     apt-get install -y wget
     if [ $? -eq 0 ]
     then
-        echo "[Wget] Успешно установлен"
+        echo "[Wget] Installation successful"
     else
-        echo "[Wget] Не удалось установить Wget. Пожалуйста, установите его вручную."
+        echo "[Wget] Failed to install Wget. Please install it manually."
         exit 1
     fi
 else
-    echo "[Wget] Установлен"
+    echo "[Wget] Installed"
 fi
 
-# Проверка установки docker.io
+# Check for docker.io installation
 if ! command -v docker > /dev/null
 then
-    echo "[Docker] Docker не найден. Попытка установить..."
+    echo "[Docker] Docker not found. Attempting to install..."
     apt-get install -y docker.io
     if [ $? -eq 0 ]
     then
-        echo "[Docker] Успешно установлен"
+        echo "[Docker] Installation successful"
     else
-        echo "[Docker] Не удалось установить Docker. Пожалуйста, установите его вручную."
+        echo "[Docker] Failed to install Docker. Please install it manually."
         exit 1
     fi
 else
-    echo "[Docker] Установлен"
+    echo "[Docker] Installed"
 fi
 
-# Проверка и установка Docker Compose
+# Check for Docker Compose installation
 if ! command -v docker-compose > /dev/null
 then
-    echo "[Docker-Compose] Docker Compose не найден. Попытка установить..."
+    echo "[Docker-Compose] Docker Compose not found. Attempting to install..."
     DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
     mkdir -p $DOCKER_CONFIG/cli-plugins
     curl -SL https://github.com/docker/compose/releases/download/v2.24.7/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
     chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
     if [ $? -eq 0 ]
     then
-        echo "[Docker-Compose] Успешно установлен"
+        echo "[Docker-Compose] Installation successful"
     else
-        echo "[Docker-Compose] Не удалось установить Docker Compose. Пожалуйста, установите его вручную."
+        echo "[Docker-Compose] Failed to install Docker Compose. Please install it manually."
         exit 1
     fi
 else
-    echo "[Docker-Compose] Установлен"
+    echo "[Docker-Compose] Installed"
 fi
 
-# Запрос ProjectName от пользователя
-while :
-do
-    printf "Название проекта: "
-    read ProjectName
-    if [ -z "$ProjectName" ]
-    then
-        echo "[Ошибка] Значение не может быть пустым. Пожалуйста, попробуйте снова."
-    else
-        break
-    fi
-done
+# Download the file
+wget https://raw.githubusercontent.com/GamerVII-NET/Gml.Backend/master/docker-compose-prod.yml
 
-# Запрос ProjectDescription от пользователя
-echo "Описание проекта: (нажмите Enter, чтобы использовать Игровой проект $ProjectName):"
-read ProjectDescription
+# Use printf to create the .env file and ask for user input for the empty variables
+printf "API_URL=http://$(hostname -I | awk '{print $1}'):5000\n\
+POSTGRES_USER=gmlcore\n\
+POSTGRES_PASSWORD=$(openssl rand -hex 16)\n\
+POSTGRES_DB=gmlcoredb\n\
+GLITCHTIP_DOMAIN=http://$(hostname -I | awk '{print $1}'):5007\n\
+GLITCHTIP_SECRET_KEY=$(openssl rand -hex 32)\n\
+ADMIN_EMAIL=$(read -p "Укажите Email-адрес администратора: " email; echo $email)\n\
+PORT_GML_BACKEND=5000\n\
+PORT_GML_FRONTEND=5003\n\
+PORT_GML_FILES=5005\n\
+PORT_GML_SENTRY=5007\n\
+PORT_GML_SKINS=5006\n" > .env
 
-# Генерация SecretKey
-SecretKey=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-
-# Запрос ProjectVersion от пользователя
-echo "Введите ProjectVersion (нажмите Enter, чтобы использовать 1.1.0):"
-read ProjectVersion
-ProjectVersion="${ProjectVersion:-1.1.0}"
-
-# Клонирование репозитория
-echo "Клонирую репозиторий..."
-git clone --recursive https://github.com/GamerVII-NET/Gml.Backend.git "$ProjectName"
-if [ $? -eq 0 ]
-then
-    echo "Репозиторий успешно клонирован"
-else
-    echo "[Ошибка] Не удалось клонировать репозиторий. Пожалуйста, проверьте ваше соединение с интернетом и доступность репозитория. Так же, убедитесь, что в текущей директории нет папки $ProjectName"
-    exit 1
-fi
-
-# Получение внешнего IP-адреса
-EXTERNAL_IP=$(curl -s ifconfig.me)
-
-# Переход к директории проекта и создание .env файла
-cd "$ProjectName"/src/Gml.Web.Client
-
-# Создание .env файла
-if [ ! -f .env ]; then
-    echo "NEXT_PUBLIC_BASE_URL=http://$EXTERNAL_IP:5000" > .env
-    echo "NEXT_PUBLIC_PREFIX_API=api" >> .env
-    echo "NEXT_PUBLIC_VERSION_API=v1" >> .env
-fi
-
-# Переход к папке $ProjectName/src/Gml.Web.Api/src/Gml.Web.Api
-cd "../../src/Gml.Web.Api/src/Gml.Web.Api/"
-
-# Удаление appsettings.Development.json
-rm -f appsettings.Development.json
-
-# Редактирование appsettings.json
-if [ -f appsettings.json ]; then
-    
-    jq ".ServerSettings.ProjectName = \"$ProjectName\" |
-    .ServerSettings.ProjectDescription = \"$ProjectDescription\" |
-    .ServerSettings.SecretKey = \"$SecretKey\" |
-    .ServerSettings.PolicyName = \"${ProjectName}Policy\" |
-    .ConnectionStrings.SQLite = \"Data Source=data.db\"" appsettings.json > temp.json && mv temp.json appsettings.json
-fi
-
-docker compose up -d
-
-echo ==================================================
-echo "\e[32mПроект успешно установлен:\e[0m"
-echo "Админпанель: http://$EXTERNAL_IP:5003/"
-echo "             *Небходима регистрация"
-echo "Управление файлами: http://$EXTERNAL_IP:5005/"
-echo "                    Логин: admin"
-echo "                    Пароль: admin"
-echo ==================================================
+# Run
+docker compose -f docker-compose-prod.yml up -d
