d4dca1847688e59c46e3ddb52097a232e6d86897
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-05-14 13:35:02 +0300

    Create gml-updater-rus.sh

---
+++ gml-updater-rus.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "[Gml] Данный скрипт нужно запускать от имени root"
+    exit 1
+fi
+
+
+# Check for docker.io installation
+if ! command -v docker >/dev/null; then
+    echo "[Gml] Docker не установлен, обновление невозможно"
+    exit 1
+fi
+
+if [ -f .env ]; then
+    echo "[Gml] Проверка .evn - \e[32mУспешно\e[0m"
+else
+    echo "[Gml] Файл .env отсутствует"
+    exit 1
+fi
+
+if [ -f docker-compose.yml ]; then
+    echo "[Gml] Проверка docker-compose.yml - \e[32mУспешно\e[0m"
+else
+    echo "[Gml] Файл docker-compose.yml отсутствует"
+    exit 1
+fi
+
+if [ -f ./frontend/Gml.Web.Client/.env ]; then
+    echo "[Gml] Проверка ./frontend/Gml.Web.Client/.env - \e[32mУспешно\e[0m"
+else
+    echo "[Gml] Файл ./frontend/Gml.Web.Client/.env отсутствует"
+    exit 1
+fi
+
+# Read the content of the file into a variable
+content=$(cat ./frontend/Gml.Web.Client/.env)
+
+docker compose down
+
+docker rmi ghcr.io/gamervii-net/gml.web.skin.service:master
+docker rmi ghcr.io/gamervii-net/gml.web.api:master
+docker rmi gml-web-frontend-image
+
+rm -Rf ./frontend
+git clone https://github.com/Scondic/Gml.Web.Client.git ./frontend/Gml.Web.Client
+
+echo "$content" > frontend/Gml.Web.Client/.env-test
+
+docker compose up -d
