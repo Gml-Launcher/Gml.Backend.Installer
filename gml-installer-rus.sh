1535b7283fae53accc68eabfdb791516d63a560c
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-06-09 07:47:14 +0300

    Update gml-installer-rus.sh

--- gml-installer-rus.sh
+++ gml-installer-rus.sh
@@ -1,98 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "Данный скрипт нужно запускать от имени root"
     exit 1
 fi
 
-#!/bin/bash
-
-# Check for git installation
-if ! command -v git >/dev/null; then
-    echo "[Git] Git not found. Attempting to install..."
-    apt-get install -y git
-    if [ $? -eq 0 ]; then
-        echo "[Git] Installation successful"
+# Check for required tools and install if necessary
+for tool in git jq curl wget docker docker-compose; do
+    if ! command -v $tool >/dev/null; then
+        echo "[$tool] $tool not found. Attempting to install..."
+        apt-get install -y $tool
+        if [ $? -eq 0 ]; then
+            echo "[$tool] Installation successful"
+        else
+            echo "[$tool] Failed to install $tool. Please install it manually."
+            exit 1
+        fi
     else
-        echo "[Git] Failed to install Git. Please install it manually."
-        exit 1
+        echo "[$tool] Installed"
     fi
-else
-    echo "[Git] Installed"
-fi
-
-# Check for jq installation
-if ! command -v jq >/dev/null; then
-    echo "[jq] jq not found. Attempting to install..."
-    apt-get install -y jq
-    if [ $? -eq 0 ]; then
-        echo "[jq] Installation successful"
-    else
-        echo "[jq] Failed to install jq. Please install it manually."
-        exit 1
-    fi
-else
-    echo "[jq] Installed"
-fi
-
-# Check for curl installation
-if ! command -v curl >/dev/null; then
-    echo "[Curl] Curl not found. Attempting to install..."
-    apt-get install -y curl
-    if [ $? -eq 0 ]; then
-        echo "[Curl] Installation successful"
-    else
-        echo "[Curl] Failed to install Curl. Please install it manually."
-        exit 1
-    fi
-else
-    echo "[Curl] Installed"
-fi
-
-# Check for wget installation
-if ! command -v wget >/dev/null; then
-    echo "[Wget] Wget not found. Attempting to install..."
-    apt-get install -y wget
-    if [ $? -eq 0 ]; then
-        echo "[Wget] Installation successful"
-    else
-        echo "[Wget] Failed to install Wget. Please install it manually."
-        exit 1
-    fi
-else
-    echo "[Wget] Installed"
-fi
-
-# Check for docker.io installation
-if ! command -v docker >/dev/null; then
-    echo "[Docker] Docker not found. Attempting to install..."
-    apt-get install -y docker.io
-    if [ $? -eq 0 ]; then
-        echo "[Docker] Installation successful"
-    else
-        echo "[Docker] Failed to install Docker. Please install it manually."
-        exit 1
-    fi
-else
-    echo "[Docker] Installed"
-fi
-
-# Check for Docker Compose installation
-if ! command -v docker-compose >/dev/null; then
-    echo "[Docker-Compose] Docker Compose not found. Attempting to install..."
-    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-    mkdir -p $DOCKER_CONFIG/cli-plugins
-    curl -SL https://github.com/docker/compose/releases/download/v2.24.7/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-    chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-    if [ $? -eq 0 ]; then
-        echo "[Docker-Compose] Installation successful"
-    else
-        echo "[Docker-Compose] Failed to install Docker Compose. Please install it manually."
-        exit 1
-    fi
-else
-    echo "[Docker-Compose] Installed"
-fi
+done
 
 # Загрузка docker-compose.yml
 wget https://raw.githubusercontent.com/GamerVII-NET/Gml.Backend/master/docker-compose-prod.yml -O docker-compose.yml
@@ -105,9 +32,16 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ -f .env ]; then
-    echo "[Gml] Файл .env существует. Исполизование локальной конфигурации..."
+    echo "[Gml] Файл .env существует. Использование локальной конфигурации..."
 else
     echo "[Gml] Файл .env не найден. Производится настройка...."
+
+    # Generate SECURITY_KEY
+    security_key=$(openssl rand -hex 32)
+    
+    echo "[Gml] Пожалуйста, введите наименование проекта:"
+    read project_name
+
     echo "[Gml] Пожалуйста, придумайте логин для S3 хранилища Minio:"
     read login_minio
 
@@ -123,11 +57,21 @@ else
     fi
 
     echo "[Gml] Gml.Web.Api настроена на использование HTTP/S: $panel_url"
+
+    # Set PROJECT_POLICYNAME
+    project_policyname=$(echo "$project_name" | tr -d '[:space:]')Policy
+
     # Создание файла .env и запись в него переменных
     echo "UID=0
 GID=0
 
-S3_ENABLED=true 
+SECURITY_KEY=$security_key
+PROJECT_NAME=$project_name
+PROJECT_DESCRIPTION=
+PROJECT_POLICYNAME=$project_policyname
+PROJECT_PATH=
+
+S3_ENABLED=true
 
 MINIO_ROOT_USER=$login_minio
 MINIO_ROOT_PASSWORD=$password_minio
@@ -139,7 +83,7 @@ MINIO_CONSOLE_ADDRESS_PORT=5010
 PORT_GML_BACKEND=5000
 PORT_GML_FRONTEND=5003
 PORT_GML_FILES=5005
-PORT_GML_SKINS=5006" >.env
+PORT_GML_SKINS=5006" > .env
 
     rm -Rf ./frontend
     git clone https://github.com/Scondic/Gml.Web.Client.git ./frontend/Gml.Web.Client
@@ -147,7 +91,7 @@ PORT_GML_SKINS=5006" >.env
     # Создание файла .env и запись в него переменных
     echo "NEXT_PUBLIC_BASE_URL=$panel_url
 NEXT_PUBLIC_PREFIX_API=api
-NEXT_PUBLIC_VERSION_API=v1" >./frontend/Gml.Web.Client/.env
+NEXT_PUBLIC_VERSION_API=v1" > ./frontend/Gml.Web.Client/.env
 
 fi
 
