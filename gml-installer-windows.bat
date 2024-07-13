417eb2b91db68f441011d7426302e1b3e1400ced
Anton Terentev <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-13 20:02:27 +0300

    Update gml-installer-windows.bat

--- gml-installer-windows.bat
+++ gml-installer-windows.bat
@@ -1,29 +1,30 @@
 @echo off
+setlocal enabledelayedexpansion
 chcp 65001 >nul
 
 :: Проверка наличия git, docker, docker-compose
 where /q git
 if ERRORLEVEL 1 (
-    echo [Git] Not found. Please install it manually.
+    echo [Git] Не найден. Установите его вручную.
     exit /b 1
 ) else (
-    echo [Git] Installed
+    echo [Git] Установлен
 )
 
 where /q docker
 if ERRORLEVEL 1 (
-    echo [Docker] Not found. Please install it manually.
+    echo [Docker] Не найден. Установите его вручную.
     exit /b 1
 ) else (
-    echo [Docker] Installed
+    echo [Docker] Установлен
 )
 
 where /q docker-compose
 if ERRORLEVEL 1 (
-    echo [Docker-Compose] Not found. Please install it manually.
+    echo [Docker-Compose] Не найден. Установите его вручную.
     exit /b 1
 ) else (
-    echo [Docker-Compose] Installed
+    echo [Docker-Compose] Установлен
 )
 
 :: Загрузка docker-compose.yml
@@ -31,18 +32,18 @@ bitsadmin /transfer "MyDownloadJob" /download /priority normal https://raw.githu
 
 :: Настройка
 IF EXIST .env (
-    echo [Gml] File .env exists. Using local configuration...
+    echo [Gml] Файл .env найден. Используется локальная конфигурация...
 ) ELSE (
-    echo [Gml] File .env not found. Setting up...
+    echo [Gml] Файл .env не найден. Настройка...
 
     :: Генерация SECURITY_KEY
     for /f "delims=" %%i in ('powershell -Command "[guid]::NewGuid().ToString()"') do set "security_key=%%i"
 
-    set /p project_name="[Gml] Please enter the project name: "
-    set /p login_minio="[Gml] Please enter a username for the S3 Minio storage: "
-    set /p password_minio="[Gml] Please enter a password for S3 Minio: "
+    set /p project_name="[Gml] Введите имя проекта: "
+    set /p login_minio="[Gml] Введите имя пользователя для хранилища S3 Minio: "
+    set /p password_minio="[Gml] Введите пароль для S3 Minio (минимум 32 символа): "
     
-    set /p panel_url="[Gml] Please enter the address to the Gml control panel, port is mandatory if you're not using proxying (Default: http://localhost:5000): "
+    set /p panel_url="[Gml] Введите адрес панели управления Gml, порт обязателен, если не используется проксирование (По умолчанию: http://localhost:5000): "
     if "%panel_url%"=="" (
         set panel_url=http://localhost:5000
     )
@@ -50,19 +51,19 @@ IF EXIST .env (
     echo [Gml] Gml.Web.Api настроена на использование HTTP/S: %panel_url%
 
     :: Удаление пробелов из project_name для project_policyname
-    set "project_policyname=%project_name: =%Policy"
+    set "project_policyname=!project_name: =%Policy"
 
     (
         echo UID=0
         echo GID=0
-        echo SECURITY_KEY=%security_key%
-        echo PROJECT_NAME=%project_name%
+        echo SECURITY_KEY=!security_key!
+        echo PROJECT_NAME=!project_name!
         echo PROJECT_DESCRIPTION=
-        echo PROJECT_POLICYNAME=%project_policyname%
+        echo PROJECT_POLICYNAME=!project_policyname!
         echo PROJECT_PATH=
         echo S3_ENABLED=true
-        echo MINIO_ROOT_USER=%login_minio%
-        echo MINIO_ROOT_PASSWORD=%password_minio%
+        echo MINIO_ROOT_USER=!login_minio!
+        echo MINIO_ROOT_PASSWORD=!password_minio!
         echo MINIO_ADDRESS=:5009
         echo MINIO_ADDRESS_PORT=5009
         echo MINIO_CONSOLE_ADDRESS=:5010
@@ -77,13 +78,13 @@ IF EXIST .env (
     git clone https://github.com/Scondic/Gml.Web.Client.git frontend/Gml.Web.Client
 
     (
-        echo NEXT_PUBLIC_BASE_URL=%panel_url%
+        echo NEXT_PUBLIC_BASE_URL=!panel_url!
         echo NEXT_PUBLIC_PREFIX_API=api
         echo NEXT_PUBLIC_VERSION_API=v1
     ) > frontend/Gml.Web.Client/.env
 )
 
-:: Run
+:: Запуск
 docker compose up -d
 
 echo.
@@ -92,7 +93,7 @@ echo ==================================================
 echo Проект успешно установлен:
 echo ==================================================
 echo Админпанель: http://localhost:5003/
-echo              *Небходима регистрация
+echo              *Необходима регистрация
 echo --------------------------------------------------
 echo Управление файлами: http://localhost:5005/
 echo                     Логин: admin
